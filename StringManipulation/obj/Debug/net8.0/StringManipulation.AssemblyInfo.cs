3582feb9218a6e7df31fd8dbd9d40edef9a2309b
Tylam.dev <tylam.dev@gmail.com>
2024-07-16 13:14:05 -0500

    Adds some types of Assert

--- StringManipulation/obj/Debug/net8.0/StringManipulation.AssemblyInfo.cs
+++ StringManipulation/obj/Debug/net8.0/StringManipulation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StringManipulation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e403ecf1220b087026565c4f4e9ecc8e5b2b38d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StringManipulation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StringManipulation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- StringManipulationTest/UnitTest1.cs
+++ StringManipulationTest/UnitTest1.cs
@@ -5,8 +5,60 @@ public class UnitTest1
     [Fact]
     public void ConcatenateString()
     {
+        //Arrange
         var strOperation = new StringOperations();
+        //Act
         var result = strOperation.ConcatenateStrings("Hello", "Platzi");
+        //Assert
+        Assert.NotNull(result);
+        Assert.NotEmpty(result);
         Assert.Equal("Hello Platzi", result);
+    }
+
+    [Fact]
+    public void IsPalindrome_True()
+    {
+        //Arrange
+        var stroperations = new StringOperations();
+        //Act
+        var result = stroperations.IsPalindrome("ana");
+        //Assert
+        Assert.True(result);
+    }
+        [Fact]
+    public void IsPalindrome_False()
+    {
+        //Arrange
+        var stroperations = new StringOperations();
+        //Act
+        var result = stroperations.IsPalindrome("hello");
+        //Assert
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void RemoveWhiteSpaceTest()
+    {
+        // Given
+        var strOperations = new StringOperations();
+        // When
+        var result = strOperations.RemoveWhitespace("sdfvsdfv");
+        // Then
+        Assert.DoesNotContain(" ", result);
+    }
+
+    [Fact]
+    public void QuantintyInWords()
+    {
+        var stroperation = new StringOperations();
+        var result = stroperation.QuantintyInWords("cat", 10);
+        Assert.StartsWith("diez", result);
+        Assert.Contains("cat", result);
+    }
+    [Fact]
+    public void GetStringLength_Exception()
+    {
+        var strOperation = new StringOperations();
+        Assert.ThrowsAny<ArgumentNullException>(() => strOperation.GetStringLength(null));
     }
 }
